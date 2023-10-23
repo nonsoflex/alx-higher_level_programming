cdaf22bb05cce35e4654699abfb30740d686be88
nonsoflex <emmflexcsc400@gmail.com>
2023-10-23 04:17:33 -0700

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,9 +2,9 @@
 
 def safe_print_list(my_list=[], x=0):
     total = 0
-    for n in range(x):
+    for i in range(x):
         try:
-            print(f"{my_list[n]}", end="")
+            print(f"{my_list[i]}", end="")
             total += 1
         except IndexError:
             break
