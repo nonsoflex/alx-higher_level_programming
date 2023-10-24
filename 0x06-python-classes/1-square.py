d4fc92ee29e6bc66aea3bed64c98c4f512ffc428
nonsoflex <emmflexcsc400@gmail.com>
2023-10-24 04:06:52 -0700

    1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-# 1-square
-""" Defines a square """ 
+
+"""Defines a square """
+
+
 class Square:
-    """ Represents a square """
+    """Represents a square"""
+
     def __init__(self, size):
         """Initializing this square class
-        Args: size - represents the size of the square defined"""
+        Args: size - represnets the size of the square defined
+        """
+
         self.__size = size
